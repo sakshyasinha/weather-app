982bb83dc086abf66b994b6673162c490e0d2004
sakshyasinha <142413749+sakshyasinha@users.noreply.github.com>
2024-12-06 11:02:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-const url =
-	'https://api.openweathermap.org/data/2.5/weather';
-const apiKey =
-	'f00c38e0279b7bc85480c3fe775d518c';
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f00c38e0279b7bc85480c3fe775d518c';
 
 $(document).ready(function () {
-	weatherFn('Delhi');
+    weatherFn('Delhi');
 });
 
 async function weatherFn(cName) {
-	const temp =
-		`${url}?q=${cName}&appid=${apiKey}&units=metric`;
-	try {
-		const res = await fetch(temp);
-		const data = await res.json();
-		if (res.ok) {
-			weatherShowFn(data);
-		} else {
-			alert('City not found. Please try again.');
-		}
-	} catch (error) {
-		console.error('Error fetching weather data:', error);
-	}
+    const temp = `${url}?q=${cName}&appid=${apiKey}&units=metric`;
+    try {
+        const res = await fetch(temp);
+        const data = await res.json();
+        if (res.ok) {
+            weatherShowFn(data);
+        } else {
+            alert('City not found. Please try again.');
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
 }
 
 function weatherShowFn(data) {
-	$('#city-name').text(data.name);
-	$('#date').text(moment().
-		format('MMMM Do YYYY, h:mm:ss a'));
-	$('#temperature').
-		html(`${data.main.temp}°C`);
-	$('#description').
-		text(data.weather[0].description);
-	$('#wind-speed').
-		html(`Wind Speed: ${data.wind.speed} m/s`);
-        $('#weather-icon').attr('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-
-	$('#weather-info').fadeIn();
+    $('#city-name').text(data.name);
+    
+   
+    const timezoneOffset = data.timezone; // in seconds
+    const localTime = new Date(Date.now() + timezoneOffset * 1000);
+    
+   
+    const options = { timeZone: 'UTC', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const formattedTime = localTime.toLocaleString('en-US', options);
+    
+  
+    $('#date').text(formattedTime);
+    
+    $('#temperature').html(`${data.main.temp}°C`);
+    $('#description').text(data.weather[0].description);
+    $('#wind-speed').html(`Wind Speed: ${data.wind.speed} m/s`);
+    $('#weather-icon').attr('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    $('#weather-info').fadeIn();
 }
